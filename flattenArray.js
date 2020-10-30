d2536425c0c458a94ff2e613c5398eeb5df456fd
tanayghosh2102 <tanay.ghosh.2102@gmail.com>
2020-10-30 16:41:49 -0400

    Identation fixed.

--- flattenArray.js
+++ flattenArray.js
@@ -1,22 +1,20 @@
 function flattenArray(arr, out) {
-	
-	// Loop through each element in arr.
-	for(let i = 0; i < arr.length; i++) {
-		if(arr[i] instanceof Array) { // Check if the element is an instance of Array type.
-			flattenArray(arr[i], out); // If it's an array recursively loop through the nested array.
-		} else {
-			out.push(arr[i]); // If it's not an array, push it to the output array.
-		}
-	}
-	
-	return out;
+    // Loop through each element in arr.
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] instanceof Array) { // Check if the element is an instance of Array type.
+            flattenArray(arr[i], out); // If it's an array recursively loop through the nested array.
+        } else {
+            out.push(arr[i]); // If it's not an array, push it to the output array.
+        }
+    }
+    return out;
 }
 
 // Test suites
-let arr1 = [1,[2,3]];
+let arr1 = [1, [2, 3]];
 let arr2 = [[1], 2, [3, [4], 5, [6, [7, [8]]]], 9];
 let arr3 = [1, [2, 3], [4, 5, [6, 7], 8, [9], [[[10]]]]];
-let arr4 = [[1],[[2]], [[[3]]], [[4]], [5]];
+let arr4 = [[1], [[2]], [[[3]]], [[4]], [5]];
 
 console.log("Arbitrarily nested arr1: ", arr1);
 console.log("Arbitrarily nested arr2: ", arr2);
